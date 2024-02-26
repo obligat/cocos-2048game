3d9a81e61eb40db1243780b1d82e61d3187d9d7a
dujinqiao <dujinqiao@corp.netease.com>
2024-02-26 11:24:40 +0800

    feat: 添加按钮点击事件游戏界面切换

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -3,12 +3,60 @@ const { ccclass, property } = _decorator;
 
 @ccclass('Game')
 export class Game extends Component {
-    start() {
 
+    @property(Node) 
+    startPanel: Node = null;
+
+    @property(Node)
+    gamePanel: Node = null;
+
+    @property(Node)
+    overPanel: Node = null;
+
+    start() {
+        this.initPanel();
+        this.startPanel.active = true;
     }
 
     update(deltaTime: number) {
         
+    }
+
+    private initPanel() {
+        this.startPanel.active = false;
+        this.gamePanel.active = false;
+        this.overPanel.active = false;
+    }
+
+    // 点击开始按钮
+    private onBtnStartClick() {
+        this.initPanel();
+        this.gamePanel.active = true;
+    }
+
+    private onBtnReplayClick() {
+        
+    }
+
+    private onBtnBackClick() {
+
+
+
+    }
+
+    private onBtnHomeClick() {
+        this.initPanel();
+        this.startPanel.active = true;
+    }
+
+
+    private onOverBtnReplayClick() {
+        this.overPanel.active = false;
+    }
+
+    private onOverBtnHomeClick() {
+        this.initPanel();
+        this.startPanel.active = true;
     }
 }
 

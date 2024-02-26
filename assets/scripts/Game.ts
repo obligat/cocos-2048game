4d437f3692a3ef5f01dbd90e65fb349fbe9403ff
dujinqiao <dujinqiao@corp.netease.com>
2024-02-26 14:42:54 +0800

    feat: 随机生成一个格子数字

--- assets/scripts/Game.ts
+++ assets/scripts/Game.ts
@@ -6,10 +6,12 @@ import {
   Node,
   Prefab,
   sys,
+  tween,
   UITransform,
   v2,
   v3,
 } from "cc";
+import { Tile } from "./Tile";
 const { ccclass, property } = _decorator;
 
 @ccclass("Game")
@@ -54,6 +56,8 @@ export class Game extends Component {
   private itemWH: number = 0;
   // 父容器宽高
   private itemParentWH: number = 0;
+  // 初始化数组
+  private array = [];
 
   start() {
     this.initPanel();
@@ -115,6 +119,82 @@ export class Game extends Component {
     this.txtScore.string = `${this.userData.score}`;
     this.txtBestScore.string = `${this.userData.bestScore}`;
     this.txtBack.string = `撤回(${this.userData.backNum})`;
+
+    // 初始化数组
+    this.initArray(level);
+    // 创建随机数组
+    this.addRandomArray();
+  }
+
+  // 初始化数组
+  private initArray(lv: number) {
+    this.array = [];
+    for (let i = 0; i < lv; i++) {
+      this.array[i] = [];
+    }
+
+    for (let i = 0; i < lv; i++) {
+      for (let j = 0; j < lv; j++) {
+        this.array[i][j] = 0;
+      }
+    }
+  }
+
+  // 空格子上随机添加数字
+  private addRandomArray() {
+    let arr_0 = [];
+    for (let i = 0; i < this.array.length; i++) {
+      for (let j = 0; j < this.array[i].length; j++) {
+        const element = this.array[i][j];
+
+        if (element === 0) {
+          arr_0.push(v2(i, j));
+        }
+      }
+    }
+
+    if (arr_0.length !== 0) {
+      let i_random = Math.floor(Math.random() * arr_0.length);
+      let ii = arr_0[i_random].x;
+      let jj = arr_0[i_random].y;
+      let randomNum = Math.random() * 10;
+      if (randomNum < 2) {
+        this.array[ii][jj] = 4;
+      } else {
+        this.array[ii][jj] = 2;
+      }
+
+      this.createItem(arr_0[i_random], this.array[ii][jj], true);
+    }
+  }
+
+  private createItem(pos, num: number, isAction = false) {
+    let posStart = v2(
+      -this.itemParentWH / 2 + this.itemWH / 2 + this.gap,
+      -this.itemParentWH / 2 + this.itemWH / 2 + this.gap
+    );
+    let item = instantiate(this.item);
+    let tile = item.getComponent(Tile);
+
+    if (tile) {
+      tile.init(num);
+    }
+
+    item.parent = this.ndParent;
+
+    let itemTf: UITransform = item.getComponent(UITransform);
+    itemTf.width = this.itemWH;
+    itemTf.height = this.itemWH;
+
+    let _x = posStart.x + (itemTf.width + this.gap) * pos.y;
+    let _y = posStart.y + (itemTf.height + this.gap) * pos.x;
+
+    item.position = v3(_x, _y, 0);
+
+    if (isAction) {
+      item.scale = v3(0, 0, 0);
+      tween(item).to(0.15, { scale: v3(1, 1, 1) }, { easing: "sineInOut" }).start();
+    }
   }
 
   addItemBg(lv: number) {
